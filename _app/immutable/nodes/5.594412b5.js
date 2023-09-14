c30571ec5650105a1fa0dc0bdd917dc1efb08e96
0xF00 <0xf00@kryolite.io>
2023-09-14 21:34:10 +0300

    Updates

---
+++ _app/immutable/nodes/5.594412b5.js
@@ -0,0 +1,1 @@
+import{s as o}from"../chunks/scheduler.e108d1fd.js";import{S as t,i as n}from"../chunks/index.c3f76b74.js";const l=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"}));class i extends t{constructor(e){super(),n(this,e,null,null,o,{})}}export{i as component,l as universal};
