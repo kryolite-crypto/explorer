4c430bbaf9835ba892b267260bf4f1deafb91d68
0xF00 <0xf00@kryolite.io>
2023-10-10 12:52:51 +0300

    Updates

--- _app/immutable/chunks/navigation.4c6bff1b.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.173e225e.js";const a=o("goto"),i=o("invalidate");export{a as g,i};
---
+++ _app/immutable/chunks/navigation.d2bb92fd.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.e91b158e.js";const a=o("goto"),i=o("invalidate");export{a as g,i};
